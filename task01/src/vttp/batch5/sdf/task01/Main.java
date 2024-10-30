02cdb1a68af25f854538970923994cdabeac7a4d
zong006 <ongzhicong@gmail.com>
2024-10-30 13:56:25 +0800

    include BikeEntry class usage

--- task01/src/vttp/batch5/sdf/task01/Main.java
+++ task01/src/vttp/batch5/sdf/task01/Main.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 
+import vttp.batch5.sdf.task01.models.BikeEntry;
+
 // Use this class as the entry point of your program
 
 public class Main {
@@ -20,47 +22,53 @@ public class Main {
 		FileReader fr = new FileReader(f);
 		BufferedReader br = new BufferedReader(fr);
 
-		ArrayList<DataPoint> data = new ArrayList<>();
-		DataPoint dataPoint = null;
+		ArrayList<BikeEntry> data = new ArrayList<>();
+		BikeEntry entry = null;
 		String lineRead = "";
 		int count = 0;
 
 		while ((lineRead = br.readLine())!=null){
 			if (count>0){
 				String[] terms = lineRead.split(",");
+				entry = new BikeEntry();
 				// season,mnth,holiday,weekday, weathersit
-				dataPoint = new DataPoint();
-				dataPoint.setSeason(Integer.parseInt(terms[0]));
-				dataPoint.setMnth(Integer.parseInt(terms[1]));
-				dataPoint.setHoliday(Integer.parseInt(terms[2]));
-				dataPoint.setWeekday(Integer.parseInt(terms[3]));
-				dataPoint.setWeathersit(Integer.parseInt(terms[4]));
-				dataPoint.setUsers(Integer.parseInt(terms[8]) + Integer.parseInt(terms[9]));
-				data.add(dataPoint);
+				entry.setSeason(Integer.parseInt(terms[0]));
+				entry.setMonth(Integer.parseInt(terms[1]));
+				entry.setHoliday(Boolean.parseBoolean(terms[2]));
+				entry.setWeekday(Integer.parseInt(terms[3]));
+				entry.setWeather(Integer.parseInt(terms[4]));
+				entry.setCasual(Integer.parseInt(terms[8]));
+				entry.setRegistered(Integer.parseInt(terms[9]));
+				data.add(entry);
 			}
 			count += 1;
 		}
-		Comparator<DataPoint> compare = Comparator.comparing(d -> d.getUsers());
+		Comparator<BikeEntry> compare = Comparator.comparing(d -> d.getCasual()+d.getRegistered());
         data.sort(compare.reversed());
 
 		String[] position = {"highest", "second highest", "third highest", "fourth highest", "fifth highest"};
-		String[] season = {"spring", "summer", "fall", "winter"};
+		String[] season = {"Spring", "Summer", "Fall", "Winter"};
 		String[] day = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 		String[] month = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		String[] weather = {"Clear, Few clouds, Partly cloudy, Partly cloudy", 
 							"Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist", 
 							"Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds", 
 							"Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog"};
-		String[] holiday = {"not a holiday", "holiday"};
-
-
 		
-
 		for (int i = 0 ; i < 5 ; i ++){
+			String holiday = "";
+			if (data.get(i).isHoliday()){
+				holiday = "holiday";
+			}
+			else {
+				holiday = "not a holiday";
+			}
+			int total = data.get(i).getCasual()+data.get(i).getRegistered();
+
 			String s = "The "+ position[i] + " (position)" +  " recorded number of cyclists was in " + season[data.get(i).getSeason()-1] + " (season)"+
-						", on a " + day[data.get(i).getWeekday()-1] + " (day)" +  " in the month of " + month[data.get(i).getMnth()-1] + " (month)" + 
-						".\nThere were a total of " + data.get(i).getUsers() + " (total)" + " cyclists. The weather was " + weather[data.get(i).getWeathersit()-1] + " (weather)" 
-						+ ".\n" + day[data.get(i).getWeekday()-1] + " (day)" + " was " + holiday[data.get(i).getHoliday()];
+						", on a " + day[data.get(i).getWeekday()-1] + " (day)" +  " in the month of " + month[data.get(i).getMonth()-1] + " (month)" + 
+						".\nThere were a total of " + total + " (total)" + " cyclists. The weather was " + weather[data.get(i).getWeather()-1] + " (weather)" 
+						+ ".\n" + day[data.get(i).getWeekday()-1] + " (day)" + " was " + holiday;
 
 			System.out.println(s);
 			System.out.println();
